37d851f60139d20a05eccad2b1b722b6a654caa9
Travis Clarkson <tk1667@gmail.com>
2018-02-26 02:25:41 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,14 +1,16 @@
 class Artist
 
-  attr_accessor :name, :genre, :songs
+  attr_accessor :name, :genres, :songs
 
   def initialize(name)
     @name = name
     @songs = []
+    @genres = []
   end
 
   def add_song(song)
     @songs << song
+    @genres << song.genre
     song.artist = self
   end
 
